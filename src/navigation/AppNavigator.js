490716d455f2591f33907d2911a634b55b058a0a
kazukiomori <kazuki1110170501@gmail.com>
2025-01-22 00:24:30 +0900

    splash画面の雛形を作成

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
+}
