33dcc32805190ab1acc6bd530ca5e0003abda5e3
kazukiomori <kazuki1110170501@gmail.com>
2025-01-26 23:29:31 +0900

    ファイルのimport

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Splash from "../screens/Splash";
+import Questions from "../screens/Questions";
 
 const Stack = createNativeStackNavigator();
 
--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Splash = () => {
+const Splash = ({ navigation }) => {
   return (
     <View>
       <Text>Splash</Text>
+
+      <Pressable onPress={() => navigation.navigate("Qustions")}>
+      <Text>問題へ</Text>
+      </Pressable>
     </View>
   )
 }
