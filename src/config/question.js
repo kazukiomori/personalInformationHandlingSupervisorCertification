a8f20e5f8508cb6382cd9842580b729b5ca781b5
kazukiomori <kazuki1110170501@gmail.com>
2025-01-30 03:10:37 +0900

    問題用のファイルを追加

---
+++ src/config/question.js
@@ -0,0 +1,26 @@
+export const questions = [
+  {
+    id: 1,
+    question: "パンはパンでも食べられないパンは？",
+    options: ["メロンパン", "クリームパン", "カレーパン", "フライパン"],
+    correctAnswer: "フライパン",
+    category: "なぞなぞ"
+  },
+  {
+    id: 2,
+    question: "日本の首都は？",
+    options: ["大阪", "札幌", "東京", "福岡"],
+    correctAnswer: "東京",
+    category: "地理"
+  },
+  {
+    id: 3,
+    question: "水の化学式は？",
+    options: ["H2O", "CO2", "O2", "NaCl"],
+    correctAnswer: "H2O",
+    category: "科学"
+  },
+  {
+
+  },
+]
